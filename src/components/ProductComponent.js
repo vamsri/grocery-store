0580c0f7c3bd59138c965f7c1b486b8407089f9a
vamshi vangari <vamsri.nov@gmail.com>
2024-05-11 17:02:27 +0530

    form validations

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -1,29 +1,27 @@
 /* eslint-disable */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
 import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchProducts } from './../features/products/productSlice';
 import { fetchCategory } from './../features/categories/categorySlice';
+import { useForm } from 'react-hook-form';
 import 'ag-grid-community/styles/ag-grid.css'; // Mandatory CSS required by the grid
 import 'ag-grid-community/styles/ag-theme-quartz.css'; // Optional Theme applied to the grid
 
-const ProductPage = () => {
-    const navigate = useNavigate();  // Replace useHistory with useNavigate
+export const Error = (msg) => (<div className='text-red-400 text-right'>{msg}</div>);
 
+const ProductPage = () => {
+    const navigate = useNavigate();  // Replace useHistory with useNavigate    
     const dispatch = useDispatch();
+    const {register, handleSubmit, formState: {errors, touchedFields, isValid}} = useForm()
+
     // const productLoading = useSelector(state => state.products.loading);
     const productEntity = useSelector(state => state.products.entities);
     // const categoryLoading = useSelector(state => state.categories.loading);
     const categoryEntity = useSelector(state => state.categories.entities);
-
-    const [name, setName] = useState('');
-    const [price, setPrice] = useState('');
-    const [description, setDescription] = useState('');
     const [category, setCategory] = useState({});
-
-    const [stock, setStock] = useState('');
 
     // Column Definitions: Defines the columns to be displayed.
     const [colDefs, setColDefs] = useState();
@@ -82,9 +80,9 @@ const ProductPage = () => {
     const getProducts = () => {
         dispatch(fetchProducts());
     };
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const { name, price, description, stock } = e.target.elements;
+    const onSubmit = async (data) => {
+        console.log('errors->', errors, isValid);
+        const { username, price, description, stock } = data;
         const token = localStorage.getItem('token');
         const headers = {
             'Authorization': `Bearer ${token}`,
@@ -92,18 +90,10 @@ const ProductPage = () => {
         };
 
         // Extract the name, price, description, and stock from the event
-        const productName = name.value;
-        const productPrice = parseFloat(price.value);
-        const productDescription = description.value;
-        const productStock = parseInt(stock.value);
-
-        // Perform submission logic here
-        console.log('Form submitted!');
-        console.log('Name:', productName);
-        console.log('Price:', productPrice);
-        console.log('Description:', productDescription);
-        console.log('Category:', category._id);
-        console.log('inventoryCount:', productStock);
+        const productName = username;
+        const productPrice = parseFloat(price);
+        const productDescription = description;
+        const productStock = parseInt(stock);
 
         try {
             const response = await axios.post('http://localhost:4001/api/products', {
@@ -121,46 +111,49 @@ const ProductPage = () => {
 
     return (
         <div className="container mx-auto p-10 h-screen overflow-scroll">
-            <div className="w-96 h-128 bg-white border-2">
+            <div className="w-128 h-128 bg-white border-2">
                 <h1 className="text-sm bg-red-400 text-white uppercase w-full mx-auto p-2">Products</h1>
-                <form onSubmit={handleSubmit} className='p-2'>
-                    <div className="mb-4 flex justify-end items-center">
+                <form onSubmit={handleSubmit(onSubmit)} className='p-2'>
+                    <div className="mb-2 flex justify-end items-center">
                         <label htmlFor="name" className="block font-medium mb-1 w-1/4">Name:</label>
                         <input
-                            type="text"
-                            id="name"
-                            className="border border-gray-300 rounded px-3 py-2 w-3/4"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
+                            className={`${errors.username && touchedFields.username ? 'border-2 border-red-300' : 'border-2 border-gray-300'} rounded px-3 py-2 w-3/4`}
                             autoComplete="off"
+                            placeholder="Enter username..."
+                            {...register('username', {required: true})}
                         />
-                    </div>
-                    <div className="mb-4 flex justify-end items-center">
+                    </div>    
+                    {errors.username && touchedFields.username && Error('Username is required')}                
+                    <div className="mb-2 flex justify-end items-center">
                         <label htmlFor="price" className="block font-medium mb-1 w-1/4">Price:</label>
                         <input
                             type="number"
                             id="price"
-                            className="border border-gray-300 rounded px-3 py-2 w-3/4"
-                            value={price}
-                            onChange={(e) => setPrice(e.target.value)}
+                            className={`${errors.price && touchedFields.price ? 'border-2 border-red-300' : 'border-2 border-gray-300'} rounded px-3 py-2 w-3/4`}
+                            placeholder="Enter Price..."
+                            {...register('price', {required: true})}
                         />
+                        
                     </div>
-                    <div className="mb-4 flex justify-end items-center">
+                    {errors.price && touchedFields.price && Error('Price is required')}
+                    <div className="mb-2 flex justify-end items-center">
                         <label htmlFor="description" className="block font-medium mb-1 w-1/4">Description:</label>
                         <textarea
                             id="description"
-                            className="border border-gray-300 rounded px-3 py-2 w-3/4"
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
+                            className={`${errors.description && touchedFields.description ? 'border-2 border-red-300' : 'border-2 border-gray-300'} rounded px-3 py-2 w-3/4`}
+                            placeholder="Enter Description..."
+                            {...register('description', {required: true})}
                         ></textarea>
+                        
                     </div>
-                    <div className="mb-4 flex justify-end items-center">
+                    {errors.description && touchedFields.description && Error('Description is required')}
+                    <div className="mb-2 flex justify-end items-center">
                         <label htmlFor="category" className="block font-medium mb-1 w-1/4">Category:</label>
                         <select
                             id="category"
-                            className="border border-gray-300 rounded px-3 py-2 w-3/4"
+                            className={`${errors.name && touchedFields.name ? 'border-2 border-red-300' : 'border-2 border-gray-300'} rounded px-3 py-2 w-3/4`}
                             value={category.name}
-                            onChange={(e) => handleCategory(e)}
+                            placeholder="Enter category..."
                         >
                             {categoryEntity.map((data) => {
                                 return (<>
@@ -169,16 +162,18 @@ const ProductPage = () => {
                             })}
                         </select>
                     </div>
-                    <div className="mb-4 flex justify-end items-center">
+                    {errors.name && touchedFields.name && Error('Name is required')}
+                    <div className="mb-2 flex justify-end items-center">
                         <label htmlFor="stock" className="block font-medium mb-1 w-1/4">Stock:</label>
                         <input
                             type="number"
                             id="stock"
-                            className="border border-gray-300 rounded px-3 py-2 w-3/4"
-                            value={stock}
-                            onChange={(e) => setStock(e.target.value)}
+                            className={`${errors.stock && touchedFields.stock ? 'border-2 border-red-300' : 'border-2 border-gray-300'} rounded px-3 py-2 w-3/4`}
+                            placeholder="Enter Stock..."
+                            {...register('stock', {required: true})}
                         />
                     </div>
+                    {errors.stock && touchedFields.stock && Error('Stock is required')}
                     <div className="flex justify-center items-center">
                         <button
                             type="submit"
