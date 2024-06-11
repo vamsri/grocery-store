bd553cdfac6673abff4013f2e35c3b312cf13f24
vamshi vangari <vamsri.nov@gmail.com>
2024-06-11 23:28:15 +0530

    delete unwanted config

--- webpack.staging.js
+++
@@ -1,7 +0,0 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.config.js');
-
-module.exports = merge(common, {
-  mode: 'production',
-  // You can add more staging-specific configurations here
-});
